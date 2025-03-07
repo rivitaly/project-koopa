8b4025650d201a43a26f41e332a681592f37e149
GumdropNinja <cartergundrum@gmail.com>
2025-03-07 17:03:23 -0600

    Enemy Animation

---
+++ Project Koopa 0/Assets/Scripts/EnemyAnimation.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class EnemyAnimation : MonoBehaviour
+{
+    //I'm not too sure if AnimationClip is what to use, need to update
+    Animator animator;
+    EnemyBehaviour enemyBehaviour;
+    EnemyBehaviour.EnemyState enemyState;
+    EnemyBehaviour.EnemyState currentAnimation = EnemyBehaviour.EnemyState.Idle;   //Default
+
+    void Start()
+    {
+        enemyBehaviour = GetComponent<EnemyBehaviour>();
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Get current state
+        enemyState = enemyBehaviour.GetEnemyState();
+
+        //If same state, do nothing
+        if (enemyState == currentAnimation) return;
+
+        //Set animator's bool state
+        animator.SetBool(currentAnimation.ToString(), false);
+        animator.SetBool(enemyState.ToString(), true);
+
+        //Switch currentAnimation to playerState
+        currentAnimation = enemyState;
+    }
+}
